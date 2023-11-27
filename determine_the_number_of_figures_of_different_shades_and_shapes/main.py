70639c8d3dfe5ca8e44649129b1fdd574e1a4447
nech14 <nech14@mail.ru>
2023-11-27 11:53:39 +0800

    fix mistake

--- determine_the_number_of_figures_of_different_shades_and_shapes/main.py
+++ determine_the_number_of_figures_of_different_shades_and_shapes/main.py
@@ -22,21 +22,23 @@ rectangles = 0
 rectangles_colors = []
 
 for region in regions:
-    r = hsv[region.bbox[0]:region.bbox[2], region.bbox[1]:region.bbox[3]]
-    if np.all(r[0][0] != np.array([0, 0, 0])):        
-        rectangles += 1        
-        rectangles_colors.extend(np.unique(r)[1:])
+    r = h[region.bbox[0]:region.bbox[2], region.bbox[1]:region.bbox[3]]
+    if np.all(r[0][0] != [0, 0, 0]):
+        rectangles += 1
+        rectangles_colors.extend(np.unique(r[0]))
     else:
         circles += 1        
-        circles_colors.extend(np.unique(r)[1:])
-    colors.extend(np.unique(r)[1:])
+        circles_colors.extend(np.unique(r[0])[1:])
+    colors.extend(np.unique(r[0]))
+
+colors = np.unique(colors)[1:]
 
 print(f"Count figures: {labeled.max()}")   
 print(f"Total colors in the picture {len(np.unique(colors))}")
 print(f"\nNumber of circles: {circles}")
 print(f"The circles have {len(np.unique(circles_colors))} colors.")
 print(f"Number of rectangles: {rectangles}")
-print(f"The rectangles have {len(np.unique(rectangles_colors))} colors.")
+print(f"The rectangles have {len(np.unique(rectangles_colors)[1:])} colors.")
 
 
 #plt.imshow(hsv)
