1c554f59694a39987161b09366401faf0b65f401
nech14 <nech14@mail.ru>
2023-12-15 23:55:07 +0800

    create Dino task

--- auto_T-Rex/main.py
+++ auto_T-Rex/main.py
@@ -1,16 +1,19 @@
 import pyautogui
 import time
+import mss
 import mss.tools
-from screeninfo import get_monitors
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage.measure import label
-from PIL import Image
 import cv2
+import keyboard as kb
 
 
 def jump():
     pyautogui.hotkey("up")
+
+def refactorJump():
+    kb.press("up")
 
 
 def sit():
@@ -18,13 +21,28 @@ def sit():
     pyautogui.keyUp('down')
 
 
+def refactorSit(t):
+    kb.press('down')
+    time.sleep(t)
+    kb.release('down')
+
+
+
+
 def get_screenshot(monitors, number_monitor):
     with mss.mss() as sct:
-        monitor = {"top": 0, "left": 0, "width": monitors[2]['width'], "height": monitors[2]['height']}
+        monitor = {"top": 0, "left": 0, "width": monitors[number_monitor]['width'], "height": monitors[number_monitor]['height']}
         sct_img = sct.grab(monitor)
 
         return sct_img
 
+
+def get_screenshot_box(t, l, w, h):
+    with mss.mss() as sct:
+        box = {"top": t, "left": l, "width": w, "height": h}
+        sct_img = sct.grab(box)
+
+        return sct_img
 
 def save_screenshot(image, name, _format='png'):  
     mss.tools.to_png(image.rgb, image.size, output=f'{name}.{_format}')
@@ -35,23 +53,14 @@ dino = {}
 
 monitors = mss.mss().monitors
 
-image = np.array(get_screenshot(monitors, 2))
+image = np.array(get_screenshot(monitors, 0))
 image_binary = image.mean(2)
 image_binary[image_binary == 249] = 0
 image_binary[image_binary > 0] = 1
-#image_binary = image_binary[456:521, 1505:1565]
-#image_binary[image_binary > 200] = 0
-#image_binary[image_binary > 0] = 1
 image_label = label(image_binary)
-#image_label[image_label != 1] = 0
 
-#np.savetxt('dino.txt', image_label, fmt='%d', delimiter=' ')
-
-plt.imshow(image_binary)
-plt.show()
 
 image_label = image_binary.astype(np.uint8)
-#template_array = np.loadtxt('dino.txt', dtype=np.uint8)
 
 template_array = plt.imread('dino.png')
 template_array = template_array.mean(2)
@@ -72,6 +81,73 @@ conturs = []
 for pt in zip(*locations[::-1]):
     conturs.append(pt)
 
-pt = conturs[0]
-plt.imshow(image[pt[1]:pt[1] + template_array.shape[0], pt[0]:pt[0] + template_array.shape[1]])
-plt.show()
+
+start_image = image_label[pt[1]:pt[1]+template_array.shape[0]-15, pt[0]+template_array.shape[1]+10:pt[0]+template_array.shape[0]*3]
+
+print('gggg')
+
+
+refactorJump()
+
+start_time  = time.time()
+coef = 0.033 #0.3
+jump_time = 0.14
+sit_time = 0.25
+time_see = 0 #0.016
+
+buf_n = 0
+buf_time = [15,    30,   40,   65,  90, 110]
+buf_coef = [0.038, 0.07,  0.19, 0.35,  0.48, 0.5]
+buf_jump = [0.146, 0.14,  0.13, 0.118, 0.115, 0.097]
+while True:
+    tim = time.time() - start_time
+    if buf_n < len(buf_time) and tim > buf_time[buf_n]:
+        coef = buf_coef[buf_n]
+        jump_time = buf_jump[buf_n]
+        print("new time jump")
+        buf_n += 1
+
+    # if t >= 15:
+    #     coef = 4.7
+    #     jump_time = 0.2
+    # elif t >= 20:
+    #     coef = 4.7
+    #     jump_time = 0.18
+
+
+    t = pt[1]+5
+    l = pt[0]+120
+    h = int(template_array.shape[0]-40)
+    w = int(template_array.shape[0] * coef)
+
+    image = np.array(get_screenshot_box(t, l, w, h))
+    image_binary = image.mean(2)
+
+    image_binary[image_binary == 249] = 0
+    image_binary[image_binary > 0] = 1
+
+    start_image = image_binary.astype(np.uint8)
+
+
+    if start_image.sum() > 4:
+        #print('jump', start_image.sum(), t, coef)
+
+        if start_image[start_image.shape[0]//2:, :].sum() > 0:
+
+            time.sleep(time_see)
+
+            refactorJump()
+            time.sleep(jump_time)
+            refactorSit(0.005)
+        else:
+            refactorSit(sit_time)
+
+        # if tim > 45:
+        #     plt.imshow(start_image)
+        #     plt.show()
+        #action.append(['j', 6])
+    else:
+        time.sleep(0.0001)
+
+
+
